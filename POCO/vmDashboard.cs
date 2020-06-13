b62de9f35d24e37ce87f8db331c611e5820250fa
Nour Omar <noordawaymeh@outlook.com>
2020-06-13 18:16:09 +0300

    Dashboord usename

--- POCO/vmDashboard.cs
+++ POCO/vmDashboard.cs
@@ -8,6 +8,7 @@ namespace POCO
 {
     public class vmDashboard
     {
+        public List<User> Username { get; set; }
         public List<Messages> vmMessagesIN { get; set; }
         public List<Messages> vmMessagesOUT { get; set; }
         public List<User> vmUser { get; set; }
